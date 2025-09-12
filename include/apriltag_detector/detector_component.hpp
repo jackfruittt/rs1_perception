b12fc1d172e9f4962cb0b5c9a4e1b0ba8d35d668
ace-rv <aceviray5@gmail.com>
2025-09-13 01:04:52 +1000

    Correct branch now - adjusted apriltag package to publish scenario messages when tag is detected

--- include/apriltag_detector/detector_component.hpp
+++ include/apriltag_detector/detector_component.hpp
@@ -22,6 +22,11 @@
 #include <rclcpp/rclcpp.hpp>
 #include <sensor_msgs/msg/image.hpp>
 
+#include <nav_msgs/msg/odometry.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <tf2/utils.hpp>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+
 namespace apriltag_detector
 {
 using Image = sensor_msgs::msg::Image;
@@ -60,6 +65,39 @@ private:
   std::size_t num_tags_detected_{0};
   pluginlib::ClassLoader<apriltag_detector::Detector> detector_loader_;
   std::shared_ptr<apriltag_detector::Detector> detector_;
+
+  /* -------------------START Not from original Apriltag detector_component.hpp------------------------- */
+  
+  // Added Variables and Functions for drone swarm handling
+  void setup_drone_subpub(int drone_id);
+  void switch_drone(int drone_id);
+  void odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg);
+  void tag_callback(const apriltag_msgs::msg::AprilTagDetectionArray::SharedPtr msg);
+ 
+  int drone_count_; // Input parameter passed in to the constructor from the launch
+  int current_drone_;
+  int tag_id;
+  nav_msgs::msg::Odometry::SharedPtr drone_odom_;
+  apriltag_msgs::msg::AprilTagDetectionArray::SharedPtr current_tag_;
+
+  // ROS2 Subscribers 
+  rclcpp::Subscription<apriltag_msgs::msg::AprilTagDetectionArray>::SharedPtr detection_sub_; // Detect /tags message 
+  rclcpp::Subscription<nav_msgs::msg::Odometry>::SharedPtr odom_sub_; // For saving xyzrpy data
+  
+  // ROS2 Publishers
+  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr scenario_detection_pub_; // Publish Scenario name, Severity Rating, Drone odom, Bool drone can respond? 
+  rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr img_pub_; // Publish image topic of bottom camera when tag detected
+
+  // msgs
+  sensor_msgs::msg::Image::SharedPtr img_msg_;
+  std_msgs::msg::String scenario_msg_;
+
+  // To Switch topics
+  std::string current_topic_img_;
+  std::string current_topic_odom_;
+  
+  /* -------------------END Not from original Apriltag detector_component.hpp------------------------- */
+
 };
 
 }  // namespace apriltag_detector
